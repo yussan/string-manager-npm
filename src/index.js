d23eb50744cf8c2d5ed74b10417af71239ad817b
yussan <yusuf.hi@students.amikom.ac.id>
2017-09-29 10:28:54 +0700

    fixed #19

--- src/index.js
+++ src/index.js
@@ -1,45 +1,35 @@
-import {objToQuery, queryToObj} from './modules/httpquery'
-import {toCamelCase} from './modules/camelcase'
-import {stripTags} from './modules/html'
-import {toSingleSpace} from './modules/spaces'
-import {truncate} from './modules/truncate'
-import {toSlug} from './modules/slug'
+import * as _httpquery from './modules/httpquery'
+import * as _camelcase from './modules/camelcase'
+import * as _html from './modules/html'
+import * as _spaces from './modules/spaces'
+import * as _truncate from './modules/truncate'
+import * as _slug from './modules/slug'
 
-function ManagerString(str) {
+function StringManager(str) {
     this.data = str
     return this.data
 }
 
 function prototypeBuilder(module) {
     return function(...args) {
-        return new ManagerString(toCamelCase(this.data, ...args))
+        return new StringManager(module(this.data, ...args))
     }
 }
 
-ManagerString.prototype.toString = function(...args) {
-    return this.data
-};
+StringManager.prototype.toString = (...args)  => { return this.data }
+StringManager.prototype.objToQuery = prototypeBuilder(_httpquery.objToQuery)
+StringManager.prototype.queryToObj = prototypeBuilder(_httpquery.queryToObj)
+StringManager.prototype.toCamelCase = prototypeBuilder(_camelcase.toCamelCase)
+StringManager.prototype.stripTags = prototypeBuilder(_html.stripTags)
+StringManager.prototype.toSingleSpace = prototypeBuilder(_spaces.toSingleSpace)
+// StringManager.prototype.truncate = prototypeBuilder(_truncate.truncate)
+StringManager.prototype.toSlug = prototypeBuilder(_slug.toSlug)
 
-ManagerString.prototype.objToQuery = prototypeBuilder(objToQuery)
-ManagerString.prototype.queryToObj = prototypeBuilder(queryToObj)
-ManagerString.prototype.toCamelCase = prototypeBuilder(toCamelCase)
-ManagerString.prototype.stripTags = prototypeBuilder(stripTags)
-ManagerString.prototype.toSingleSpace = prototypeBuilder(toSingleSpace)
-ManagerString.prototype.truncate = prototypeBuilder(truncate)
+export {objToQuery, queryToObj} from './modules/httpquery'
+export {toCamelCase} from './modules/camelcase'
+export {stripTags} from './modules/html'
+export {toSingleSpace} from './modules/spaces'
+export {truncate} from './modules/truncate'
+export {toSlug} from './modules/slug'
 
-export {
-    objToQuery,
-    queryToObj,
-    toCamelCase,
-    stripTags,
-    toSingleSpace,
-    truncate,
-    toSlug
-}
-
-export default (str) => {
-    return new ManagerString(str);
-}
-
-    
-}
+export default str => { return new StringManager(str)}
