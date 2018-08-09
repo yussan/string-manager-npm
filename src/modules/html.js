9c1a4e0ee9ea984e75673525333d80f322d9b63a
yussan <yussandeveloper@gmail.com>
2018-08-09 13:40:03 +0700

    merapikan

--- src/modules/html.js
+++ src/modules/html.js
@@ -1,27 +1,21 @@
-export function stripTags(str='')
-{
-    try
-    {
-        if(!str || typeof str != 'string') return str
-        return str.replace(/(<([^>]+)>)/ig,'')
-    }catch(e)
-    {
-        console.error(e)
-    }
+export function stripTags(str = '') {
+  try {
+    if (!str || typeof str != 'string') return str
+    return str.replace(/(<([^>]+)>)/gi, '')
+  } catch (e) {
+    console.error(e)
+  }
 }
 
 /**
  * @description function to convert html \n to <br/>
  * @param {string} str , default is empty string / ''
  */
-export function nl2br(str='') 
-{
-    try
-    {
-        if(!str || typeof str != 'string') return str
-        return str.replace(/\n/ig,'<br/>')
-    }catch(e)
-    {
-        console.error(e)
-    }
+export function nl2br(str = '') {
+  try {
+    if (!str || typeof str != 'string') return str
+    return str.replace(/\n/gi, '<br/>')
+  } catch (e) {
+    console.error(e)
+  }
 }
