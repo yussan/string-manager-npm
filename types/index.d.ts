d342107da87c805125e6ca62ce5d41b6faf91daf
adeaye <ade_a_yusfantri@linkaja.id>
2022-07-19 11:07:46 +0700

    fix: Adding module type and adding exported member

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,48 +1,35 @@
-// export as namespace stringManager;
-
-// interface HisakoPayloadI {
-//   baseUrl: string;
-//   initialIV: string;
-//   initialSecret: string;
-//   storageIV?: string;
-//   storageSecret?: string;
-//   storageName?: string;
-// }
-// interface AuthPayloadI {
-//   token: string;
-//   iv: string;
-//   secret: string;
-// }
-
-// interface HisakoServices {
-//   toCamelCase: (str: string) => string;
-//   objToQuery: (obj: Object) => string;
-//   queryToObj: (str: string) => Object;
-//   stripTags: (str: string) => string;
-//   nl2br: (str: string) => string;
-//   toSingleSpace: (str: string) => string;
-//   truncate: (str: string, limit: number, suffix: string) => string;
-//   toSlug: (str: string) => string;
-//   currencyFormat: (number: number, symbol: string, delimiter: string) => string;
-//   toMask: (str: string, start: number, end: number) => string;
-//   scaleNumber: (number: number) => number;
-// }
-
-// export default function hisakoFunc(payload:HisakoPayloadI): HisakoServices
-
 declare module 'string-manager' {
+  export const toCamelCase: (str: string) => string
+  export const objToQuery: (obj: Object) => string
+  export const queryToObj: (str: string) => Object
+  export const stripTags: (str: string) => string
+  export const nl2br: (str: string) => string
+  export const toSingleSpace: (str: string) => string
+  export const truncate: (str: string, limit: number, suffix: string) => string
+  export const toSlug: (str: string) => string
+  export const currencyFormat: (
+    number: number,
+    symbol: string,
+    delimiter: string
+  ) => string
+  export const toMask: (str: string, start: number, end: number) => string
+  export const scaleNumber: (number: number) => number
   interface StringManagerI {
-    toCamelCase: (str: string) => string;
-    objToQuery: (obj: Object) => string;
-    queryToObj: (str: string) => Object;
-    stripTags: (str: string) => string;
-    nl2br: (str: string) => string;
-    toSingleSpace: (str: string) => string;
-    truncate: (str: string, limit: number, suffix: string) => string;
-    toSlug: (str: string) => string;
-    currencyFormat: (number: number, symbol: string, delimiter: string) => string;
-    toMask: (str: string, start: number, end: number) => string;
-    scaleNumber: (number: number) => number;
+    toCamelCase: (str: string) => string
+    objToQuery: (obj: Object) => string
+    queryToObj: (str: string) => Object
+    stripTags: (str: string) => string
+    nl2br: (str: string) => string
+    toSingleSpace: (str: string) => string
+    truncate: (str: string, limit: number, suffix: string) => string
+    toSlug: (str: string) => string
+    currencyFormat: (
+      number: number,
+      symbol: string,
+      delimiter: string
+    ) => string
+    toMask: (str: string, start: number, end: number) => string
+    scaleNumber: (number: number) => number
   }
   const stringManager: StringManagerI = {
     toCamelCase,
@@ -55,7 +42,8 @@ declare module 'string-manager' {
     toSlug,
     currencyFormat,
     toMask,
-    scaleNumber
+    scaleNumber,
   }
-  export default stringManager;
+
+  export default stringManager
 }
